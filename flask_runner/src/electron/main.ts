8028af4e10b24f97d04f036d8f76446ae38c5691
Jean-Philipp Bruehwiler <jpbruehwiler@gmail.com>
2025-11-14 22:00:21 +0100

    changed config items

--- flask_runner/src/electron/main.ts
+++ flask_runner/src/electron/main.ts
@@ -1,7 +1,9 @@
 import {app, BrowserWindow} from 'electron'
 import path from 'path'
 
+//type test = string;
+
 app.on("ready", () => {
     const mainWindow = new BrowserWindow({})
-    mainWindow.loadFile(path.join(app.getAppPath(), 'dist-front/index.html'))
+    mainWindow.loadFile(path.join(app.getAppPath(), 'dist-react/index.html'))
 })
--- flask_runner/vite.config.ts
+++ flask_runner/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     })
   ],
   build: {
-    outDir: "dist-front"
+    outDir: "dist-react"
   },
   base: './'
 })
