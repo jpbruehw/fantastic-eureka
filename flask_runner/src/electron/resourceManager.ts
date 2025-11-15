7de89c437755df07961dc90201231d8779268bf6
Jean-Philipp Bruehwiler <jpbruehwiler@gmail.com>
2025-11-15 19:58:25 +0100

    more functions

--- flask_runner/src/electron/resourceManager.ts
+++ flask_runner/src/electron/resourceManager.ts
@@ -1,10 +1,45 @@
-const POLLING_INTERVAL = 500
 import osUtils from 'os-utils'
+import fs from 'fs'
+import os from 'os'
+import { BrowserWindow } from 'electron'
+const POLLING_INTERVAL = 500
 
-export function pollResource() {
-    setInterval(() => {getCpuUsage()}, POLLING_INTERVAL)
+export function pollResource(mainWindow: BrowserWindow) {
+    setInterval(async () => { 
+        const cpuUsage = await getCpuUsage()
+        const ramUsage = getRamUsage()
+        const storageData = getStorageData()
+        mainWindow.webContents.send("statistics", {cpuUsage, ramUsage, storageData: storageData.total})
+    }, POLLING_INTERVAL)
+}
+
+export function getStaticData() {
+    const totalStorage = getStorageData().total;
+    const cpuModel = os.cpus()[0].model
+    const totalMemoryGB = Math.floor(osUtils.totalmem() / 1024)
+
+    return {
+        totalStorage,
+        cpuModel,
+        totalMemoryGB
+    }
 }
 
 function getCpuUsage() {
-    osUtils.cpuUsage((percentage) => console.log(percentage))
+    return new Promise(resolve => osUtils.cpuUsage(resolve))
+}
+
+function getRamUsage() {
+    return 1 - osUtils.freememPercentage()
+}
+
+function getStorageData() {
+    const stats = fs.statfsSync(process.platform == 'win32' ? 'c://' : '/')
+    const total = stats.bsize * stats.blocks
+    const free = stats.bsize * stats.bfree
+
+    return {
+        total: Math.floor(total / 1_000_000_000),
+        usage: 1 - free / total
+    }
 }
--- flask_runner/src/ui/App.tsx
+++ flask_runner/src/ui/App.tsx
@@ -1,7 +1,11 @@
+import { useEffect } from 'react'
 import './App.css'
 
 function App() {
 
+    useEffect(() => {
+        window.electron.subscribeStatistics(stats => console.log(stats))
+    })
   return (
     <>
     ten
