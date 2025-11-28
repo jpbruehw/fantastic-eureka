0c4aae8d8bf8724262863e56d9061d05fa0f6b4a
Jean-Philipp Bruehwiler <jpbruehwiler@gmail.com>
2025-11-28 21:59:24 +0100

    more testing logic

--- flask_runner/e2e/example.spec.ts
+++ flask_runner/e2e/example.spec.ts
@@ -1,18 +1,58 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, _electron } from "@playwright/test";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+/** this adds some extra config to the testing
+ *  and ensures that electron has actually booted up
+ *  when doing tests
+ *  make a note of this and make sure to keep this in future stuff
+ */
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+let electronApp: Awaited<ReturnType<typeof _electron.launch>>;
+let mainPage: Awaited<ReturnType<typeof electronApp.firstWindow>>;
+
+async function waitForPreloadScript() {
+	return new Promise((resolve) => {
+		const interval = setInterval(async () => {
+			const electronBridge = await mainPage.evaluate(() => {
+				// eslint-disable-next-line @typescript-eslint/no-explicit-any
+				return (window as Window & { electron?: any }).electron;
+			});
+			if (electronBridge) {
+				clearInterval(interval);
+				resolve(true);
+			}
+		}, 100);
+	});
+}
+
+test.beforeEach(async () => {
+	electronApp = await _electron.launch({
+		args: ["."],
+		env: { NODE_ENV: "development" },
+	});
+
+	mainPage = await electronApp.firstWindow();
+	await waitForPreloadScript();
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test.afterEach(async () => {
+	await electronApp.close();
+});
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+test("custom frame should minimize the mainWindow", async () => {
+	await mainPage.click("#minimize");
+	const isMinimized = await electronApp.evaluate((electron) => {
+		return electron.BrowserWindow.getAllWindows()[0].isMinimized();
+	});
+	expect(isMinimized).toBeTruthy();
+});
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test("should create a custom menu", async () => {
+	const menu = await electronApp.evaluate((electron) => {
+		return electron.Menu.getApplicationMenu();
+	});
+	expect(menu).not.toBeNull();
+	expect(menu?.items).toHaveLength(2);
+	expect(menu?.items[0].submenu?.items).toHaveLength(2);
+	expect(menu?.items[1].submenu?.items).toHaveLength(3);
+	expect(menu?.items[1].label).toBe("View");
 });
